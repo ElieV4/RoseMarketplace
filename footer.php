eba9b823dcd9a3d8f3bbb8df2f83cf1c899ef898
ElieV4 <elie.vincent4@gmail.com>
2024-01-07 18:44:29 +0100

    no chatbox

--- footer.php
+++ footer.php
@@ -6,24 +6,6 @@
 </head>
 <body>
 
-<button class="questionmark">
-        <div class="bar"></div>
-    </button>
-
-<div class="chatbox">
-        <div class="chat-header">
-            Chat en direct
-            <span class="close-chat">&times;</span>
-        </div>
-        <div class="chat-content">
-            <!-- Contenu de la boîte de chat -->
-        </div>
-        <div class="chat-input">
-            <input type="text" placeholder="Tapez votre message...">
-            <button>Envoyer</button>
-        </div>
-    </div>
-
     <footer class="footer">
         <br><br>
         <div class="terms"><a target="_blank" href="confidentialite.php" id="privacy-policy" class="qa-privacypolicy-link" rel="noopener">Politique de confidentialité</a> | <a href="conditions-generales.html" id="terms-and-conditions" class="qa-tandc-link" target="_blank" rel="noopener">Termes et Conditions</a><p>Copyright © ROSE. 2023<br></p></div>
