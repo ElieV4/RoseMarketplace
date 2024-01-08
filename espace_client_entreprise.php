89d264debef28e8967c7e51f44457ebbf4b73cb7
ElieV4 <elie.vincent4@gmail.com>
2024-01-08 13:28:38 +0100

    a

--- espace_client_entreprise.php
+++ espace_client_entreprise.php
@@ -79,7 +79,7 @@
                 else if(isset($_GET['tri-fact'])){
                     include('dashboard/factures.php');
                 } else {
-                    include('dashboard/messagerie.php');
+                    include('dashboard/produits_stocks.php');
                 }
             ?>
         </div>
