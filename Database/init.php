9eb2e5afea4816d170680da728bf8c9fac6ab63b
ElieV4 <elie.vincent4@gmail.com>
2024-01-08 15:34:48 +0100

    final

--- Database/init.php
+++ Database/init.php
@@ -13,20 +13,14 @@ if ($mysqli->connect_error) {
 
 // Création de la base de données
 $sql = file_get_contents("createdatabase_marketplace.sql");
-    if ($mysqli->multi_query($sql)) {
-    // première requête succès
+if ($mysqli->multi_query($sql)) {
     if ($mysqli->errno === 0) {
         $con = mysqli_connect('localhost', 'root', '', 'rosemarketplace');
         if (!$con) {
             die(mysqli_error($con));
         } else {
-            $sql2 = file_get_contents("test.sql");
-            if ($result = mysqli_query($con, $sql2)) {
-                echo 'Base de données rosemarketplace créée avec succès !.<br>';
-                echo '<a href="../index.php">Cliquez sur le lien suivant pour accéder au site</a>';
-            } else {
-                echo "Erreur lors de l'insertion des photos :))) : " . $mysqli->error;
-            }
+            echo 'Base de données rosemarketplace créée avec succès !<br>';
+            echo '<a href="../index.php">Cliquez sur le lien suivant pour accéder au site</a>';
         }
     } else {
         echo "Erreur lors de la création de la base de données : " . $mysqli->error;
